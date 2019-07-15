cc9b07bbb13a6cd21f2843a1139238815a3f4141
Ashiq <ashikul58bd@gmail.com>
2019-07-16 00:06:29 +0600

    damage

--- app/Http/Controllers/DamageController.php
+++ app/Http/Controllers/DamageController.php
@@ -3,8 +3,64 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Damage;
 
 class DamageController extends Controller
 {
-    //
+    public function damages()
+    {
+    	return view('admin.damage.index');
+    }
+ 
+ 	public function damagesAll()
+    {
+        $damages = Damage::with('inventory')->get();
+        return response()->json(["damages"=>$damages]);
+    }
+    public function addOrUpdate(Request $request)
+    {
+        //validate data
+        $validator = \Validator::make($request->damage, [
+            'inventory_id'=>'required',
+            'issue_date'=>'required|string',
+            'reason'=>'required|string',
+            'status'=>'required|string',
+        ]);
+
+
+        if ($validator->fails()) {
+            return response()->json(['success' =>false , 'errors'=>$validator->messages()]);
+        }
+
+        if($request->damage['id'] == null){  
+
+            // create
+
+
+            $damage = Damage::create([
+            	'inventory_id' => $request->damage['inventory_id'],
+                'issue_date' => $request->damage['issue_date'],
+                'reason' => $request->damage['reason'],
+                'status' => $request->damage['status'],
+            ]);
+
+             $damage = Damage::with('account')->find($damage->id);
+
+            return response()->json(["success"=>true, 'status'=>'created', 'damage'=>$damage]);
+        } else { 
+            $damage = Damage::find($request->damage['id']);   
+            if(!$damage) return response()->json(["success"=>true, 'status'=>'somethingwrong']);        
+         
+            //update
+            $damage->update([
+          	 'inventory_id' => $request->damage['inventory_id'],
+                'issue_date' => $request->damage['issue_date'],
+                'reason' => $request->damage['reason'],
+                'status' => $request->damage['status'],
+            ]);
+            $damage = Damage::with('inventory')->find($damage->id);
+
+            return response()->json(["success"=>true, 'status'=>'updated', 'damage'=>$damage]);
+        }
+    }
 }
--- resources/views/admin/damage/index.blade.php
+++ resources/views/admin/damage/index.blade.php
@@ -14,17 +14,19 @@ Home page
 @endsection
 
 @section('content')
-<div class="row" id="expense">
+<div class="row" id="damage">
     <div class="col-12">
         <div class="card">
             <div class="card-body">
                 <div class="d-flex no-block align-items-center mb-4">
-                    <h4 class="card-title">Expense List</h4>
+                    <h4 class="card-title">Damage List</h4>
                     <div class="ml-auto">
                         <div class="btn-group">
+                          <div class="btn-group">
                             <button type="button" class="btn btn-dark" data-toggle="modal" data-target="#createmodel"  @click.prevent="clearData()">
-                            Create New Expense
+                            Create New Damage
                             </button>
+                          </div>
                         </div>
                     </div>
                 </div>
@@ -40,42 +42,33 @@ Home page
                 <thead>
                     <tr>
                          <th>S/L</th>
-                        <th>Account name</th>
-                        <th>Title</th>
-                        <th>Expense Date</th>
-                        <th>Amount</th>
-                        <th>Reason</th>
-                        <th>type</th>
+                        <th>Unique Code</th>
+                        <th>Issue Dae</th>
+                        <th>Reson</th>
+                        <th>Status</th>
+            
                         <th>Action</th>
                     </tr>
                 </thead>
                 <tfoot>
                     <tr>
-                         <th>S/L</th>
-                        <th>Account name</th>
-                        <th>Title</th>
-                        <th>Expense Date</th>
-                        <th>Amount</th>
-                        <th>Reason</th>
-                        <th>type</th>
+                        <th>S/L</th>
+                        <th>Unique Code</th>
+                        <th>Issue Dae</th>
+                        <th>Reson</th>
+                        <th>Status</th>
+                 
                         <th>Action</th>
                     </tr>
                 </tfoot>
                 <tbody>
                 <tbody>
-                    <tr v-for="(expense, index) in expenses">
+                    <tr v-for="(damage, index) in damages">
                         <td>@{{index+1}}</td>
-                        <td>@{{expense.account?expense.account.name:''}}</td>
-                        <td>@{{expense.title}}</td>
-                        <td>@{{expense.expense_date}}</td>
-                        <td>@{{expense.amount}}</td>
-                        <td>@{{expense.reason}}</td>
-                        <td>@{{expense.type}}</td>
-                        
-
-      
-
-
+                        <td>@{{damage.inventory?damage.inventory.unique_code:''}}</td>
+                        <td>@{{damage.issue_date}}</td>
+                        <td>@{{damage.reason}}</td>
+                        <td>@{{damage.status}}</td>
                         <td> 
                             <button class="btn btn-info btn-icon-split"  data-toggle="modal" data-target="#supplyDetail" @click="setData(index)">
                                 <span class="icon text-white" >
@@ -115,7 +108,7 @@ Home page
             <div class="modal-content">
                 <form action="javascript:void(0)" method="POST">
                     <div class="modal-header">
-                        <h5 class="modal-title" id="createModalLabel"><i class="ti-marker-alt mr-2"></i> Create New Expense</h5>
+                        <h5 class="modal-title" id="createModalLabel"><i class="ti-marker-alt mr-2"></i> Create New damage</h5>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                         </button>
@@ -133,66 +126,49 @@ Home page
                             <div class="col-6">
                                 <div class="input-group mb-3">
                                     <button type="button" class="btn btn-info"><i class="ti-wand text-white"></i></button>
-                                    <select class="form-control form-white" v-model="expense.account_id">
-                                        <option>select Account Name</option>
-                                        <option v-for="account in accounts" :value="account.id">@{{account.name}}</option>
+                                    <select class="form-control form-white" v-model="damage.inventory_id">
+                                        <option>select Inventory</option>
+                                        <option v-for="inventory in inventories" :value="inventory.id">@{{inventory.unique_code}}</option>
                                     </select>
                                 </div>
                             </div>
                             <div class="col-6">
                                 <div class="input-group mb-3">
                                     <button type="button" class="btn btn-info"><i class="ti-user text-white"></i></button>
-                                    <input type="text" class="form-control" placeholder="Title" v-model="expense.title" required="">
-                                    <input type="hidden" class="form-control" v-model="expense.id">
+                                    <input type="date" class="form-control" placeholder="Issue Date" v-model="damage.issue_date" required="">
+                                    <input type="hidden" class="form-control" v-model="damage.id">
                                 </div>
                             </div>
                             <div class="col-6">
                                 <div class="input-group mb-3">
                                     <button type="button" class="btn btn-info"><i class="ti-user text-white"></i></button>
-                                    <input type="date" class="form-control" placeholder="Expense Date" v-model="expense.expense_date" required="">
+                                    <input type="text" class="form-control" placeholder="Reson" v-model="damage.reason" required="">
                                     
                                 </div>
                             </div>
                             <div class="col-6">
                                 <div class="input-group mb-3">
                                     <button type="button" class="btn btn-info"><i class="ti-user text-white"></i></button>
-                                    <input type="text" class="form-control" placeholder="Amount" v-model="expense.amount" required="">
+                                    <input type="text" class="form-control" placeholder="Status" v-model="damage.status" required="">
                                     
-                                </div>
-                            </div>
-                            <div class="col-6">
-                                <div class="input-group mb-3">
-                                    <button type="button" class="btn btn-info"><i class="ti-user text-white"></i></button>
-                                    <input type="text" class="form-control" placeholder="Reason" v-model="expense.reason" required="">
-                                    
-                                </div>
-                            </div>
-                             <div class="col-6">
-                                <div class="input-group mb-3">
-                                    <button type="button" class="btn btn-info"><i class="ti-wand text-white"></i></button>
-                                    <select class="form-control form-white" v-model="expense.type">
-                                        <option value="unusual">unusual</option>
-                                        <option value="regular">regular</option>
-                                    </select>
                                 </div>
                             </div>
 
 
-                         
                         </div>
-                        <!-- <div class="input-group mb-3">
+                         <div class="input-group mb-3">
                             <button type="button" class="btn btn-info"><i class="ti-import text-white"></i></button>
                             <div class="custom-file">
                                 <input type="file" class="custom-file-input" id="inputGroupFile01">
                                 <label class="custom-file-label" for="inputGroupFile01">Choose Image</label>
                             </div>
-                        </div> -->
+                        </div> 
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-dismiss="modal" id="modalClose">Close</button>
                         <button type="submit" class="btn btn-info" @click.prevent="clearData()"><i class="ti-close"></i> Clear data</button>
-                        <button type="submit" class="btn btn-success" @click.prevent="saveData()" v-if="!expense.id"><i class="ti-save"></i> Save</button>
-                        <button type="submit" class="btn btn-primary" @click.prevent="saveData()" v-if="expense.id"><i class="ti-save"></i> Update</button>
+                        <button type="submit" class="btn btn-success" @click.prevent="saveData()" v-if="!damage.id"><i class="ti-save"></i> Save</button>
+                        <button type="submit" class="btn btn-primary" @click.prevent="saveData()" v-if="damage.id"><i class="ti-save"></i> Update</button>
                     </div>
                 </form>
             </div>
@@ -205,7 +181,7 @@ Home page
             <div class="modal-dialog modal-lg" role="document">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title" id="employeeDetailLabel">Expense detail</h5>
+                        <h5 class="modal-title" id="employeeDetailLabel">damage detail</h5>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">×</span>
                         </button>
@@ -218,36 +194,18 @@ Home page
                                     <div class="card-body">
                                         <div class="row">
                                             
-                                            <div class="col-md-3 col-xs-6 b-r"> <strong>Account Name</strong>
+                                            <div class="col-md-3 col-xs-6 b-r"> <strong>Unique Code</strong>
                                                 <br>
-                                                <p class="text-muted">@{{expense.account?expense.account.name:''}}</p>
-                                            </div><div class="col-md-3 col-xs-6 b-r"> <strong>Title</strong>
+                                                <p class="text-muted">@{{damage.inventory?damage.inventory.unique_code:''}}</p>
+                                            </div><div class="col-md-3 col-xs-6 b-r"> <strong>Issue Date</strong>
                                                 <br>
-                                                <p class="text-muted">@{{expense.title}}</p>
+                                                <p class="text-muted">@{{damage.issue_date}}</p>
                                             </div>
-                                             <div class="col-md-3 col-xs-6 b-r"> <strong>Expense Date</strong>
+                                             <div class="col-md-3 col-xs-6 b-r"> <strong>damage Date</strong>
                                                 <br>
-                                                <p class="text-muted">@{{expense.expense_date}}</p>
+                                                <p class="text-muted">@{{damage.reason}}</p>
                                             </div>
-                                            
-                                            <div class="col-md-3 col-xs-6 b-r"> <strong>Amount</strong>
-                                                <br>
-                                                <p class="text-muted">@{{expense.amount}}</p>
-                                            </div> 
-                                            <div class="col-md-3 col-xs-6 b-r"> <strong>Reason</strong>
-                                                <br>
-                                                <p class="text-muted">@{{expense.reason}}</p>
-                                            </div>
-                                            <div class="col-md-3 col-xs-6 b-r"> <strong>Type</strong>
-                                                <br>
-                                                <p class="text-muted">@{{expense.type}}</p>
-                                            </div>
-
-
-
-
-
-
+                                        
                                         </div>
 
                                     </div>
@@ -279,25 +237,23 @@ Home page
     <script src="{{asset('/')}}/js/axios.min.js"></script>
     <script type="text/javascript">
         const app = new Vue({
-            el: '#expenses',
+            el: '#damage',
             data: {
                 errors: [],
-                expense: {
+                damage: {
                     id: '',
-                    account_id:'1',
-                    title: '',
-                    expense_date: '',
-                    amount: '',
+                    inventory_id:'',
+                    issue_date: '',
                     reason: '',
-                    type: '1',
-                    account: {
+                    status: '',
+                    inventories: {
                         id:'',
-                        name: ''
+                        unique_code: ''
                     }
                 },
                 currentIndex: 0,
-                expenses: [],               
-                accounts: [],               
+                damages: [],               
+                inventories: [],               
                 successMessage:'',
             },
             mounted() {
@@ -308,17 +264,17 @@ Home page
             methods: {
                 getAllAccountData() {
                     var _this = this;
-                    axios.get('{{ route("accounts.all") }}')
+                    axios.get('{{ route("inventories.all") }}')
                     .then(function (response) {
-                        _this.accounts = response.data.accounts;
+                        _this.inventories = response.data.inventories;
                     })
                 },
 
                 getAllData(){
                       var _this = this;
-                    axios.get('{{ route("expenses.all") }}')
+                    axios.get('{{ route("damages.all") }}')
                     .then(function (response) {
-                        _this.expenses = response.data.expenses;
+                        _this.damages = response.data.damages;
                     })
 
                 },
@@ -326,16 +282,16 @@ Home page
                     var _this = this;
                     _this.errors = [];
                     _this.currentIndex = index;
-                    _this.expense = _this.expenses[index];
+                    _this.damage = _this.damages[index];
                 },
                 clearData() {
                     var _this = this;
                     _this.errors = [];
-                    _this.expense = {
+                    _this.damage = {
                         id: '',
                         account_id: '1',
                         title:'',
-                        expense_date: '',
+                        damage_date: '',
                         amount: '',
                         reason: '',
                         type: '1',
@@ -353,9 +309,9 @@ Home page
                     if(_this.validate()){
                         //save data
                         let data = {
-                            expense: _this.expense
+                            damage: _this.damage
                         }
-                        axios.post('{{ route("expenses.addOrUpdate") }}', data)
+                        axios.post('{{ route("damages.addOrUpdate") }}', data)
                         .then(function (response) {
                             let data = response.data;
                             let status = response.data.status;
@@ -366,17 +322,17 @@ Home page
                                     alert("something Wrong. Try Again.")
                                 }
                                 if(status=='created') {
-                                    _this.expenses.push(data.expense);
+                                    _this.damages.push(data.damage);
                                     //modal close
                                     document.getElementById('modalClose').click();
-                                    _this.successMessage = 'expense created successfully';
+                                    _this.successMessage = 'damage created successfully';
                                 }
                                 if(status=='updated') {
                                     
-                                    _this.$set( _this.expenses, _this.currentIndex, data.expense )
+                                    _this.$set( _this.damages, _this.currentIndex, data.damage )
                                     //modal close
                                     document.getElementById('modalClose').click();
-                                    _this.successMessage = 'expense updated successfully';
+                                    _this.successMessage = 'damage updated successfully';
                                 }
                             } else {                                
                                 for (var key in data.errors) {
@@ -391,31 +347,31 @@ Home page
                 validate() {           
                     var _this = this; 
                     _this.errors = [];
-                    let expense = _this.expense;
+                    let damage = _this.damage;
                     let count = 0; 
 
-                    if (!expense.title) {
+                    if (!damage.title) {
                         _this.errors.push("Title required.");
                         count++;
                     }
-                     if (!expense.expense_date) {
-                        _this.errors.push("Expense Date required.");
+                     if (!damage.damage_date) {
+                        _this.errors.push("damage Date required.");
                         count++;
                     }
-                    if (!expense.amount) {
+                    if (!damage.amount) {
                         _this.errors.push("Amount required.");
                         count++;
                     }
-                    if (!expense.reason) {
+                    if (!damage.reason) {
                         _this.errors.push("Reason required.");
                         count++;
                     }
-                    if (!expense.type) {
+                    if (!damage.type) {
                         _this.errors.push("Type required.");
                         count++;
                     }
 
-                     if (!expense.account_id) {
+                     if (!damage.account_id) {
                         _this.errors.push("Account name required.");
                         count++;
                     }
