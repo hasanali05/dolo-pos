5929cd39a8ba7f11366c8b77bd1d0381624a2883
Md Hasan Ali <hasan.bubt.40@gmail.com>
2019-08-29 15:22:53 +0600

    customer from sale

--- resources/views/admin/sales/create.blade.php
+++ resources/views/admin/sales/create.blade.php
@@ -242,6 +242,9 @@ Home page
                                 <option v-for="(customer,index) in customers" :value="index" :key="index">@{{ customer.name }}</option>
                             </optgroup>
                         </select>
+                        <button type="button" class="btn btn-dark w-100" data-toggle="modal" data-target="#createCustomerModel"  @click.prevent="clearCustomerData()">
+                        Create New
+                        </button>
                     </div>
                     <div class="col-sm-12 p-t-10 p-b-10" >        
                         <label style="font-weight: 500">customer information</label>
@@ -299,6 +302,142 @@ Home page
             </div>
         </div>
     </div>
+    
+
+<div class="modal fade" id="createCustomerModel" tabindex="-1" role="dialog" aria-labelledby="createModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg" role="document">
+            <div class="modal-content">
+                <form action="javascript:void(0)" method="POST">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="createModalLabel"><i class="ti-marker-alt mr-2"></i> Create New customer</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+
+
+                        <div v-if="errors.length" class="alert alert-danger">
+                            <b>Please correct the following error(s):</b>
+                            <ul>
+                                <li v-for="error in errors">@{{ error }}</li>
+                            </ul>
+                        </div>
+                        <div class="row">
+                            <div class="col-6">
+                                <div class="input-group mb-3">
+                                    <button type="button" class="btn btn-info"><i class="fas fa-angle-double-down"></i></button>
+                                    <select class="form-control form-white" v-model="customer.account_id">
+                                        <option>select Account Name</option>
+                                        <option v-for="account in accounts" :value="account.id">@{{account.name}}</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="col-6">
+                                <div class="input-group mb-3">
+                                    <button type="button" class="btn btn-info"><i class="ti-user text-white"></i></button>
+                                    <input type="text" class="form-control" placeholder="name" v-model="customer.name" required="">
+                                    <input type="hidden" class="form-control" v-model="customer.id">
+                                </div>
+                            </div>
+                            <div class="col-6">
+                                <div class="input-group mb-3">
+                                    <button type="button" class="btn btn-info"><i class="fas fa-phone"></i></button>
+                                    <input type="text" class="form-control" placeholder="contact" v-model="customer.contact" required="">
+                                    
+                                </div>
+                            </div>
+                            <div class="col-6">
+                                <div class="input-group mb-3">
+                                    <button type="button" class="btn btn-info"><i class="far fa-address-card"></i></button>
+                                    <input type="text" class="form-control" placeholder="Address" v-model="customer.address" required="">
+                                    
+                                </div>
+                            </div>
+
+
+                            <div class="col-6">
+                                <div class="input-group mb-3">
+                                    <button type="button" class="btn btn-info"><i class="ti-wand text-white"></i></button>
+                                    <select class="form-control form-white" v-model="customer.is_active">
+                                        <option value="1">Active</option>
+                                        <option value="0">In-active</option>
+                                    </select>
+                                </div>
+                            </div>                         
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal" id="modalClose">Close</button>
+                        <button type="submit" class="btn btn-info" @click.prevent="clearCustomerData()"><i class="ti-close"></i> Clear data</button>
+                        <button type="submit" class="btn btn-success" @click.prevent="saveCustomerData()" v-if="!customer.id"><i class="ti-save"></i> Save</button>
+                        <button type="submit" class="btn btn-primary" @click.prevent="saveCustomerData()" v-if="customer.id"><i class="ti-save"></i> Update</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+        
+    </div>
+
+
+            <div class="modal fade" tabindex="-1" role="dialog" aria-labelledby="employeeDetailLabel" aria-modal="true" id="supplyDetail">
+            <div class="modal-dialog modal-lg" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="employeeDetailLabel">Supply detail</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">×</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="row">
+                            <!-- Column -->
+                            <div class="col-lg-12 col-xlg-12 col-md-12">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <div class="row">  
+                                            <div class="col-md-3 col-xs-6 b-r"> <strong>Account Name</strong>
+                                                <br>
+                                                <p class="text-muted">@{{customer.account?customer.account.name:''}}</p>
+                                            </div><div class="col-md-3 col-xs-6 b-r"> <strong>Name</strong>
+                                                <br>
+                                                <p class="text-muted">@{{customer.name}}</p>
+                                            </div>
+                                             <div class="col-md-3 col-xs-6 b-r"> <strong>Mobile</strong>
+                                                <br>
+                                                <p class="text-muted">@{{customer.contact}}</p>
+                                            </div>
+                                            <div class="col-md-3 col-xs-6 b-r"> <strong>Status</strong>
+                                                <br>
+                                                <h3>
+                                                    <span class="badge badge-success" v-if="customer.is_active == 1">Active
+                                                    </span>
+                                                    <span class="badge badge-danger" v-else>Inactive
+                                                    </span>
+                                                </h3>
+                                            </div>
+                                            <div class="col-md-6 col-xs-6 b-r"> <strong>Address</strong>
+                                                <br>
+                                                <p class="text-muted">@{{customer.address}}</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Column -->
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal"  v-on:click="counter += 1" >Close</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    
+
+
+</div>
+
 </div>
 
 @endsection
@@ -315,6 +454,19 @@ Home page
             data: {
                 errors: [],
                 successMessage:'',
+                customer: {
+                    id: '',
+                    account_id:'',
+                    name: '',
+                    contact: '',
+                    address: '',
+                    is_active: '1',
+                    account: {
+                        id:'',
+                        name: ''
+                    }
+                },
+                accounts: JSON.parse('{!!$receivableAccounts!!}'),    
 
                 customers: [],
                 products: [],
@@ -364,6 +516,123 @@ Home page
                 _this.getInventoryProduct();
             },
             methods: {
+              
+                clearCustomerData() {
+                    var _this = this;
+                    _this.errors = [];
+                    _this.customer = {
+                        id: '',
+                        name: '',
+                        account_id: '',
+                        contact: '',
+                        address: '',
+                        is_active: '1',
+                        account:{
+                            id:'',
+                            name:''
+                        }
+                    }
+                },
+                
+                saveCustomerData() {
+                    var _this = this;
+                    if(_this.validateCustomer()){
+                        //save data
+                        let data = {
+                            customer: _this.customer
+                        }
+                        axios.post('{{ route("customers.addOrUpdate") }}', data)
+                        .then(function (response) {
+                            let data = response.data;
+                            let status = response.data.status;
+                            if(response.data.success == true) {
+                                //modal close
+                                if (status=='somethingwrong') {
+                                    // _this.errors.push("Something wrong. Try again.");
+                                    alert("something Wrong. Try Again.")
+                                }
+                                if(status=='created') {
+                                    _this.customers.push(data.customer);
+                                    //modal close
+                                    document.getElementById('modalClose').click();
+                                   
+                                      //sweet alrat
+
+                                    const Toast = Swal.mixin({
+                                      toast: true,
+                                      position: 'top-end',
+                                      showConfirmButton: false,
+                                      timer: 3000
+                                  });
+
+                                    Toast.fire({
+                                      type: 'success',
+                                      title: 'Customer created successfully'
+                                  })
+
+                                    //end sweet alart
+                                }
+                                if(status=='updated') {
+                                    
+                                    _this.$set( _this.customers, _this.currentIndex, data.customer )
+                                    //modal close
+                                    document.getElementById('modalClose').click();
+                                    
+                                      //sweet alrat
+
+                                    const Toast = Swal.mixin({
+                                      toast: true,
+                                      position: 'top-end',
+                                      showConfirmButton: false,
+                                      timer: 3000
+                                  });
+
+                                    Toast.fire({
+                                      type: 'success',
+                                      title: 'Customer updated successfully'
+                                  })
+
+                                    //end sweet alart
+                                }
+                                
+                                _this.getAllcustomer();
+                            } else {                                
+                                for (var key in data.errors) {
+                                    data.errors[key].forEach(function(element) {
+                                        _this.errors.push(element);
+                                    });
+                                }
+                            }
+                        }) 
+                    }
+                },
+                validateCustomer() {           
+                    var _this = this; 
+                    _this.errors = [];
+                    let customer = _this.customer;
+                    let count = 0; 
+
+                    if (!customer.name) {
+                        _this.errors.push("Name required.");
+                        count++;
+                    }
+                     if (!customer.contact) {
+                        _this.errors.push("Contact required.");
+                        count++;
+                    }
+                    if (!customer.address) {
+                        _this.errors.push("Address required.");
+                        count++;
+                    }
+
+                     if (!customer.account_id) {
+                        _this.errors.push("Account name required.");
+                        count++;
+                    }
+
+                    if(count==0) return true;
+                    else return false;
+                },
                 getAllcustomer() {
                     var _this = this;
                     axios.get('{{ route("customers.all") }}')
